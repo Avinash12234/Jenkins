eeffaba5d65f91c19728a4161efe31a18b562d06
Avinash12234 <106006127+Avinash12234@users.noreply.github.com>
2023-01-10 16:20:21 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -13,8 +13,8 @@ public class prime {
     }
 
     if (!flag)
-      System.out.println(num + " is a prime number.");
+      System.out.println(num + " is a Prime number.");
     else
-      System.out.println(num + " is not a prime number.");
+      System.out.println(num + " is not a Prime number.");
   }
 }
