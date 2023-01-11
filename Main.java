a736b0a246eb4b19118cb2250564bd9cd8f4a5c7
Avinash12234 <106006127+Avinash12234@users.noreply.github.com>
2023-01-11 11:44:05 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
   public static void main(String[] args) {
-    int rows = 6;
+    int rows = 8;
 
     for (int i = 1; i <= rows; ++i) {
       for (int k = 1; k <= i; ++k) {
