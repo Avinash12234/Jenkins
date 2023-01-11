c78a323a56ace7ebcb9b961008871ceef2e0aa87
Avinash12234 <106006127+Avinash12234@users.noreply.github.com>
2023-01-11 15:50:57 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -3,7 +3,7 @@ public class prime {
   public static void main(String[] args) {
 
     int num = 49;
-    boolean flag =false;
+    boolean flag = false;
     for (int i = 2; i <= num / 2; ++i) {
       // condition for nonprime number
       if (num % i == 0) {
